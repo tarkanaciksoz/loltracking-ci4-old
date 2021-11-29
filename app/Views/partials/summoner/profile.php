564d8d327288ced2b89ce67b3d368ef9ed4d9440
tarkanaciksoz <tarkanaciksoz@gmail.com>
2021-11-30 02:21:35 +0300

    profile view improved

--- app/Views/partials/summoner/profile.php
+++ app/Views/partials/summoner/profile.php
@@ -1,36 +1,71 @@
 <div class="container px-5">
     <img class="pageBanner card-img-top" src="http://lolg-cdn.porofessor.gg/img/d/champion-banners/14.jpg"/>
     <div class="card">
-        <div class="card-body">
-            <div class="col-auto">
-                <table>
-                    <thead>
-                    <tr>
-                        <th>
-                            <img class="profile-icon" src="http://ddragon.leagueoflegends.com/cdn/11.23.1/img/profileicon/588.png"/>
-                        </th>
-                        <th>
-                            <h3 class="align-text-top">Muckmeister</h3>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td colspan="2">
-                            <h3 class="align-text-top">123 level</h3>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <!--  class="col-12 text-start" style="height: 33.333333%;" -->
+                    <div class="col-12 text-start">
+                        <img class="profile-icon" src="http://ddragon.leagueoflegends.com/cdn/11.23.1/img/profileicon/588.png">
+                    </div>
+                    <div class="col-12 text-center m-auto">
+                        Muckmeister
+                    </div>
+                    <div class="col-12 text-start">
+                        123 Level
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="col-12 text-center">
+                        Ranked solo duo
+                    </div>
+                    <div class="row">
+                        <div class="col-4 text-center">
+                            win
+                        </div>
+                        <div class="col-4 text-center m-auto">
+                            <img src="https://loltracking.tarkanaciksoz.tech/dist/assets/img/Emblem_Platinum.png" style="width: 70px; height: 80px;">
+                        </div>
+                        <div class="col-4 text-center">
+                            lose
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4 text-center">
+                            16
+                        </div>
+                        <div class="col-4 text-center"></div>
+                        <div class="col-4 text-center">
+                            12
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="col-12 text-center">
+                        Ranked Flex mlex
+                    </div>
+                    <div class="row">
+                        <div class="col-4 text-center">
+                            win
+                        </div>
+                        <div class="col-4 text-center m-auto">
+                            <img src="https://loltracking.tarkanaciksoz.tech/dist/assets/img/Emblem_Platinum.png" style="width: 70px; height: 80px;">
+                        </div>
+                        <div class="col-4 text-center">
+                            lose
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4 text-center">
+                            16
+                        </div>
+                        <div class="col-4 text-center"></div>
+                        <div class="col-4 text-center">
+                            12
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<!--<div class="card">
-    <img class="card-img-top" src="http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Sion_0.jpg" alt="Card image cap">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>-->
