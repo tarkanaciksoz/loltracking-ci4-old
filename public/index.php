678bb545a59224b0b7114b1da9864eda39290db1
tarkanaciksoz <tarkanaciksoz@gmail.com>
2021-11-29 05:07:11 +0300

    cart

--- public/index.php
+++ public/index.php
@@ -18,6 +18,7 @@ chdir(__DIR__);
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
 $pathsConfig = FCPATH . '../app/Config/Paths.php';
+//echo $pathsConfig; exit;
 // ^^^ Change this if you move your application folder
 require realpath($pathsConfig) ?: $pathsConfig;
 
