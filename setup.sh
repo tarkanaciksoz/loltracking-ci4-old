fb373a1eb6d298ba64d37f2cc5f0047e53ae3e6f
tarkanaciksoz <tarkan.aciksoz@modanisa.com>
2022-05-19 03:48:12 +0300

    spark-serve fix

--- setup.sh
+++
@@ -1,6 +0,0 @@
-composer install 
-composer update 
-rm ./spark 
-rm ./public/index.php 
-cp vendor/codeigniter4/codeigniter4/spark ./ 
-cp vendor/codeigniter4/codeigniter4/public/index.php ./public/
