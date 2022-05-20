d75c068c665229d0dcd49356613b7b2e588e06c1
tarkanaciksoz <tarkan.aciksoz@modanisa.com>
2022-05-20 05:58:19 +0300

    fix

--- app/Views/partials/summoner/profile.php
+++ app/Views/partials/summoner/profile.php
@@ -119,7 +119,7 @@
 <footer class="py-5 bg-black">
     <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Your Website 2022</p></div>
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js?v=<?= Constants::ASSET_VERSION; ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?= $distDir; ?>js/scripts.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
